5007a8b655c9fa99e11c51df67820784794f6871
JenMcDonald <68691434+JenMcDonald@users.noreply.github.com>
2020-10-04 21:06:28 +0100

    Add files via upload

---
+++ precache-manifest.6c4fea6d89b2dd79e233fba4b96e8181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61842a917598a198e02af31a4a598100",
+    "url": "/React-Madlib/index.html"
+  },
+  {
+    "revision": "a8f0c2759af3ecacfca5",
+    "url": "/React-Madlib/static/css/main.ec5f27b0.chunk.css"
+  },
+  {
+    "revision": "584603ec8e715be8facf",
+    "url": "/React-Madlib/static/js/2.978f79bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Madlib/static/js/2.978f79bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f0c2759af3ecacfca5",
+    "url": "/React-Madlib/static/js/main.3023dab3.chunk.js"
+  },
+  {
+    "revision": "392e4469f281c7d9d131",
+    "url": "/React-Madlib/static/js/runtime-main.17f24434.js"
+  },
+  {
+    "revision": "0a103e9f73f916d0bc69b103b6f82875",
+    "url": "/React-Madlib/static/media/ship.0a103e9f.jpg"
+  }
+]);

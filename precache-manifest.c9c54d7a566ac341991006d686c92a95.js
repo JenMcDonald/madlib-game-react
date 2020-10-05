fe2e6f43ce8ce6814d32aaf47a7d0177eb119308
JenMcDonald <68691434+JenMcDonald@users.noreply.github.com>
2020-10-05 10:46:54 +0100

    Updates

---
+++ precache-manifest.c9c54d7a566ac341991006d686c92a95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4000a7de74b7e0cdb32ee4cd15986e87",
+    "url": "/madlib-game-react/index.html"
+  },
+  {
+    "revision": "350382f0f0f7ef9e2bb5",
+    "url": "/madlib-game-react/static/css/main.951570de.chunk.css"
+  },
+  {
+    "revision": "643e354aaf4e27eb0903",
+    "url": "/madlib-game-react/static/js/2.1adf7065.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/madlib-game-react/static/js/2.1adf7065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350382f0f0f7ef9e2bb5",
+    "url": "/madlib-game-react/static/js/main.6a578f80.chunk.js"
+  },
+  {
+    "revision": "6bd6cf8830ae0cb6814a",
+    "url": "/madlib-game-react/static/js/runtime-main.e5728269.js"
+  },
+  {
+    "revision": "0a103e9f73f916d0bc69b103b6f82875",
+    "url": "/madlib-game-react/static/media/ship.0a103e9f.jpg"
+  }
+]);
